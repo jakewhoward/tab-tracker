c0e4b131553ad7c1171399e0ff275cff529caab5
Jake Howard <jake.howard@Jake-H.local>
2017-10-30 16:31:18 -0700

    Creating sercer

---
+++ server/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+		"extends": "standard",
+		'rules': {
+			"strict": 0,
+			"indent": ["error", "tab"],
+			"comma-dangle": 2,
+			"no-unused-vars": [2, {"args": "none"}],
+			"no-console": 1,
+			"no-var": 2,
+			"no-tabs": 0,
+			"space-before-function-paren": [2, { "anonymous": "always", "named": "never" }],
+			"space-infix-ops": 0,
+			"max-len": [2, 160, 2],
+			"padded-blocks": 0,
+			"no-multiple-empty-lines": 2
+		}
+};

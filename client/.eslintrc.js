e35ab132a6a9a37a72179bd6bf8cd7bab2ea6ce9
Jake Howard <jake.howard@Jake-H.local>
2017-10-30 16:30:24 -0700

    Updating eslint config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,27 +1,33 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+	root: true,
+	parser: 'babel-eslint',
+	parserOptions: {
+		sourceType: 'module'
+	},
+	env: {
+		browser: true,
+	},
+	// https://github.com/standard/standard/blob/master/docs/RULES-en.md
+	extends: 'standard',
+	// required to lint *.vue files
+	plugins: [
+		'html'
+	],
+	// add your custom rules here
+	'rules': {
+		"strict": 0,
+		"indent": ["error", "tab"],
+		"comma-dangle": 2,
+		"no-unused-vars": [2, {"args": "none"}],
+		"no-console": 1,
+		"no-var": 2,
+		"no-tabs": 0,
+		"space-before-function-paren": [2, { "anonymous": "always", "named": "never" }],
+		"space-infix-ops": 0,
+		"max-len": [2, 160, 2],
+		"padded-blocks": 0,
+		"no-multiple-empty-lines": 2
+	}
 }
